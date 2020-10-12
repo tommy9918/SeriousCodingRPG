737cf11845fff603e06fabf4023cfb2d25973445
tommy9918 <tommy.101@hotmail.com>
2020-10-12 23:26:26 +0800

    Updated player data

--- Serious Coding RPG/Assets/Scripts/General/Player/PlayerData.cs
+++ Serious Coding RPG/Assets/Scripts/General/Player/PlayerData.cs
@@ -12,19 +12,20 @@ public class PlayerData
     }
     public Gender gender;
     public string name;
-    
+    public List<int> completedTask;
 
     public PlayerData()    //initialize a new player
     {
         gender = Gender.Male;
         name = "Nameless wizard";
+        completedTask = new List<int>();
     }
 
     public PlayerData(Player player)    //get data from player
     {
         this.gender = player.data.gender;
         this.name = player.data.name;
-        this.completedTask = new List<int>();
+        this.completedTask = new List<int>(player.data.completedTask);
     }
 
     
