ad9ab60d98dcac94609258e628638af2f1a25206
tommy9918 <tommy.101@hotmail.com>
2020-10-27 19:55:58 +0800

    Character Manager Added

---
+++ Serious Coding RPG/Assets/Scripts/General/Player/CharacterManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTouchDown()
+    {
+
+    }
+
+    void OnTouchStay()
+    {
+
+    }
+
+    void OnTouchUp()
+    {
+
+    }
+
+    void OnTouchExit()
+    {
+
+    }
+}
