14cac4b1f1307fc8bf0c0685b16d0e6e04c96eaa
potechtohk <potechtohk@gmail.com>
2021-01-16 19:11:24 +0800

    firebase v0.1

---
+++ Serious Coding RPG/Assets/Firebase/FirebaseInit.cs
@@ -0,0 +1,22 @@
+﻿using Firebase;
+// using Firebase.Analytics;
+using UnityEngine;
+
+public class FirebaseInit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync();
+        // FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        // {
+        //     FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        // });
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
